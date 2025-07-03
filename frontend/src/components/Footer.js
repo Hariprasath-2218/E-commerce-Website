9cd11d25d731fbc345cc1e245eb3f2de2c3bb858
Hariprasath-2218 <aileaner2218@gmail.com>
2025-07-03 16:07:15 +0530

    footer Update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,8 @@ export default function Footer() {
     return (
         <footer className="py-1 bg-dark">
             <p className="text-center text-white mt-1 ">
-                JVLcart - 2023-2024, All Rights Reserved
+                &copy; 2025
+                amazon.co, All Rights Reserved
             </p>
         </footer>
     )
